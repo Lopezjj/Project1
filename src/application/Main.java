812b76ad25c7dd7ff1b6ba9de4eaac18ecd0a245
KyleOGrady <ogradyks@clarkson.edu>
2016-11-07 15:18:57 -0500

    Add files via upload

---
+++ src/application/Main.java
@@ -0,0 +1,38 @@
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.beans.value.ObservableValue;
+import javafx.event.*;  // Contains EventHandler & ActionEvent
+import javafx.scene.input.*; // Contains MouseEvent & KeyEvent
+import javafx.stage.*; // Contains Stage and StageStyle
+import javafx.scene.*; // Contains Scene, Group, and Node
+import javafx.scene.control.*; // Contains Button
+import javafx.scene.image.*; // Contains ImageView & Image
+import javafx.scene.layout.*; // Contains subclasses of anchorPane
+import javafx.scene.text.Text;
+
+public class Main extends Application {
+
+	Scene startMenu, setGamePlay, playThree, playFive, playSeven;
+	
+	
+	
+	public void start(Stage window){
+	
+	
+	
+	
+	
+		
+		
+		
+		
+		
+		
+	
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
